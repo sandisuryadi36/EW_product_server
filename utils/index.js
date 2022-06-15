46f5dc9e5138bb1ba08f38c1c1ee34a70866ddea
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-06-15 18:10:14 +0700

    test cors

--- utils/index.js
+++ utils/index.js
@@ -9,6 +9,7 @@ function getToken(req) {
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
         return req.headers.authorization.split(' ')[1];
     }
+    console.log(req.cookies);
     return req.cookies.token;
 }
 
