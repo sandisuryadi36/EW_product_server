1b8262de13163b2f149873b49bf28cc2612786af
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-06-16 15:34:18 +0700

    test cookie

--- utils/index.js
+++ utils/index.js
@@ -9,7 +9,7 @@ function getToken(req) {
     // if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
     //     return req.headers.authorization.split(' ')[1];
     // }
-    console.log(req)
+    console.log(req.headers)
     return req.signedCookies.token;
 }
 
